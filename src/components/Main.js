8a14d0294d30e1561bbc68590832958d05fb2432
tuhnik <huntlarsen@gmail.com>
2019-02-09 13:59:52 +0200

    css

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
+let tags = ["Tere", "Hola", "Yo", "Random"]
+
 
 class Main extends Component {
   render() {
     return (
     <div className="main">
       <aside>
+      <div className="headline">Tags</div>
+        <div className="header-line"></div>
         <div className="tag-list">
-          <div className="tag">Tere</div>
-          <div className="tag">Hola</div>
-          </div>
+        {tags.map((el, i)=>{
+          return <div key={i}className="tag">{el}</div>
+        })}
+        </div>
       </aside>
       <article>
         <div className="headline">All stories</div>
