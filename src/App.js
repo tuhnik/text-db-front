04a34349d099a6f6e78cfea7cd549741640938b2
tuhnik <huntlarsen@gmail.com>
2019-02-09 19:42:35 +0200

    ...

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useStore } from 'easy-peasy';
 import Header from './components/Header'
-import Main from './components/Main'
 import Footer from './components/Footer'
 import Routes from "./Routes";
 
