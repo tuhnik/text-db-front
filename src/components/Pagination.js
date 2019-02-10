bdf8a5d52644340e7a32131cbfc2742919853a33
tuhnik <huntlarsen@gmail.com>
2019-02-10 17:16:01 +0200

    pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -87,14 +87,12 @@ function Pagination(props){
 const range = (from, to, step = 1) => {
     let i = from;
     const range = [];
-  
     while (i <= to) {
       range.push(i);
       i += step;
     }
-  
     return range;
-  }
+}
   
 
 
