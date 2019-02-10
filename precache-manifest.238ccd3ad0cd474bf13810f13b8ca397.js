264daa55e8a1015b0a124e35eff077c2b93c4004
tuhnik <huntlarsen@gmail.com>
2019-02-10 19:08:48 +0200

    Updates

--- precache-manifest.238ccd3ad0cd474bf13810f13b8ca397.js
+++ precache-manifest.238ccd3ad0cd474bf13810f13b8ca397.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/text-db-front/static/js/runtime~main.4d250ad2.js"
   },
   {
-    "revision": "090b79e01ff8f1056702",
-    "url": "/text-db-front/static/js/main.090b79e0.chunk.js"
+    "revision": "350ed98fb257f13408b2",
+    "url": "/text-db-front/static/js/main.350ed98f.chunk.js"
   },
   {
     "revision": "cee3385e110ea447f21a",
     "url": "/text-db-front/static/js/1.cee3385e.chunk.js"
   },
   {
-    "revision": "090b79e01ff8f1056702",
-    "url": "/text-db-front/static/css/main.4b90019f.chunk.css"
+    "revision": "350ed98fb257f13408b2",
+    "url": "/text-db-front/static/css/main.6dcc0931.chunk.css"
   },
   {
-    "revision": "e5b1f01ad0a1cf88164cbfe036b2d733",
+    "revision": "b8cf7ab14f990dbf41559ec94cf20d91",
     "url": "/text-db-front/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/text-db-front/precache-manifest.84892602f5794adcf9f28598e887eb82.js"
+  "/text-db-front/precache-manifest.238ccd3ad0cd474bf13810f13b8ca397.js"
 );
 
 workbox.clientsClaim();
