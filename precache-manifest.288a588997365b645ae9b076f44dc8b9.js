3ab3edae8254368577f4a298372276b91aa97bb8
tuhnik <huntlarsen@gmail.com>
2019-02-10 17:18:50 +0200

    Updates

---
+++ precache-manifest.288a588997365b645ae9b076f44dc8b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d250ad21c2c9abd9c66",
+    "url": "/text-db-front/static/js/runtime~main.4d250ad2.js"
+  },
+  {
+    "revision": "05140c45ce48da58eb5c",
+    "url": "/text-db-front/static/js/main.05140c45.chunk.js"
+  },
+  {
+    "revision": "cee3385e110ea447f21a",
+    "url": "/text-db-front/static/js/1.cee3385e.chunk.js"
+  },
+  {
+    "revision": "05140c45ce48da58eb5c",
+    "url": "/text-db-front/static/css/main.bab4cb2b.chunk.css"
+  },
+  {
+    "revision": "cc8d15572d615e382d2927d6d185343c",
+    "url": "/text-db-front/index.html"
+  }
+];
